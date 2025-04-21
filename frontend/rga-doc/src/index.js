5cda9d478530b7e614ab0cc92f3c056805e2c192
amit <amit.prajapati@lpu.in>
2025-04-21 23:50:16 +0530

    .

--- frontend/rga-doc/src/index.js
+++ frontend/rga-doc/src/index.js
@@ -2,21 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-// Create a root element for the React app
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Render the app, wrapped in BrowserRouter with the basename
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/Document-Management-and-RAG-based-Q-A-Application">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
